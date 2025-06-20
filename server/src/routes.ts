30d061390b0feac8ba3214159083ab640d7b3794
Ivan Loseev <loseev.ivan@eesee.io>
2025-06-20 08:46:32 +0300

    chore: fix s3

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -2,7 +2,7 @@ import { Router } from 'express'
 import { body } from 'express-validator'
 import { UserController } from './controllers/auth.controller.ts'
 import { S3Controller } from './controllers/s3.controller.ts'
-import { authMiddleware, multerMiddleware } from './lib/index.ts'
+import { multerMiddleware } from './lib/index.ts'
 
 export const router: Router = Router()
   .post('/login', body('email').isEmail(), (req, res, next) => {
@@ -14,9 +14,5 @@ export const router: Router = Router()
   .get('/refresh', (req, res, next) => {
     UserController.refresh(req, res, next)
   })
-  .put('/s3', [
-    authMiddleware,
-    multerMiddleware,
-    S3Controller.uploadBinaryFiles
-  ])
+  .put('/s3', [multerMiddleware, S3Controller.uploadBinaryFiles])
   .get('/s3/:key', S3Controller.getBinaryFile)
