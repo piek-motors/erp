f1743a379c389ba97014db9a9670fbf85834b950
Ivan Loseev <loseev.ivan@eesee.io>
2025-07-11 23:50:57 +0300

    fix: deploy script

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -1,37 +1,26 @@
 #!/bin/bash
-# Build client locally
-echo "Building client app..."
-cd ./client || exit 1
-npm run build:release || { echo "Client build failed"; exit 1; }
-cd ..
-# Commit build artifacts and push to repo
-echo "Committing and pushing build artifacts..."
-git add client/build || true
-git commit -m "deploy" || true
-git push
-
-# Choose your target host (you can pass 'namegs' or 'piek' as argument)
-TARGET="piek"
-# Remote directory to deploy
-REMOTE_DIR="/home/prod/erp"  # <-- change this
-# PM2 process name
-PM2_PROCESS_NAME="erp"  # <-- change this
+TARGET="piek-office" # SSH target host 
+REMOTE_DIR="/home/fin/apps/erp" # Remote directory to deploy
+PM2_PROCESS_NAME="erp" # PM2 process name
 # Connect and deploy
 ssh "$TARGET" <<EOF
   set -e  # exit on any error
   cd "$REMOTE_DIR"
   echo "Pulling latest code..."
   git pull
-  pnpm i --frozen-lockfile
+  
+  export PATH="\$HOME/.local/share/pnpm:\$HOME/.nvm/versions/node/v23.9.0/bin:\$PATH"
 
-  cd ~/erp/domain-model
+  pnpm i --frozen-lockfile
   pnpm build
 
-  cd ~/erp/db
+  cd ~/apps/erp/client
+  pnpm build:release
+
+  cd ~/apps/erp/db
   pnpm migrate
 
-  cd ~/erp/server
-  
+  cd ~/apps/erp/server
   if npx pm2 describe "$PM2_PROCESS_NAME" > /dev/null; then
     echo "Process exists. Restarting..."
     npx pm2 restart "$PM2_PROCESS_NAME"
