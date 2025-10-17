0ce7bf90422921aa2e14372238007e273c32913b
Ivan Loseev <loseev.ivan@eesee.io>
2025-10-17 15:47:58 +0300

    fix: err handling

--- server/src/lib/trpc/trpc.ts
+++ server/src/lib/trpc/trpc.ts
@@ -4,14 +4,11 @@ import type { Context } from './context.ts'
 
 const t = initTRPC.context<Context>().create({
   errorFormatter({ shape, error, input }) {
-  errorFormatter({ shape, error, input }) {
     if (error.message === 'jwt expired') {
       return
     }
     log.error(
       {
-        code: shape.data.code,
-        rpc: shape.data.path,
         input
       },
       `${error.name} rpc=${shape.data.path} code=${shape.data.code}: ${error.message}`
