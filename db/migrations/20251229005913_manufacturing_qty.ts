7bc9aa676cc31a692c9cbf36df4c85c80ce6786b
Ivan <loseev5@gmail.com>
2025-12-30 03:45:10 +0300

    fix: migration

--- db/migrations/20251229005913_manufacturing_qty.ts
+++ db/migrations/20251229005913_manufacturing_qty.ts
@@ -2,14 +2,13 @@ import { sql } from 'kysely'
 import { type KDB } from '../schema'
 
 export async function up(db: KDB): Promise<void> {
+  await sql`
+      ALTER TABLE pdo.manufacturing RENAME TO orders;
+    `.execute(db)
   await db.schema
     .alterTable('pdo.orders')
     .addColumn('output_qty', 'integer')
     .execute()
-
-  await sql`
-    ALTER TABLE pdo.manufacturing RENAME TO pdo.orders;
-  `.execute(db)
 }
 
 export async function down(db: KDB): Promise<void> {
