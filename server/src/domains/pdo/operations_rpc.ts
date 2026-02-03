e0e324399b4a6e372642b3af6405ff8f082555d8
Ivan <loseev5@gmail.com>
2026-02-03 16:55:36 +0300

    fix: operaitons type inference collisions

--- server/src/domains/pdo/operations_rpc.ts
+++ server/src/domains/pdo/operations_rpc.ts
@@ -2,22 +2,22 @@ import { matrixEncoder } from '#root/lib/matrix_encoder.js'
 import { formatDate } from '#root/lib/time.js'
 import { router } from '#root/lib/trpc/trpc.js'
 import { db, procedure, z } from '#root/sdk.js'
-import type { DB, Selectable } from 'db'
-import { type Unit } from 'models'
+import { OperationType, SupplyReason, Unit, WriteoffReason } from 'models'
 
 const Limit = 100
 
-export type OperationListItem = Omit<
-	Selectable<DB.OperationsTable>,
-	'timestamp'
-> & {
-	timestamp: string | null
-	detail_name: string | null
-	detail_group_id: number | null
-	material_label: string | null
-	manufacturing_order_id: number | null
-	manufacturing_order_qty: number | null
+export type OperationListItem = {
+	operation_type: OperationType
+	timestamp: string
+	qty: number
+	manufacturing_order_id: number
+	reason: SupplyReason | WriteoffReason
 	unit: Unit
+	detail_id: number
+	detail_name: string
+	detail_group_id: number
+	material_id: number
+	manufacturing_order_qty: number
 }
 
 export const operations = router({
@@ -37,15 +37,15 @@ export const operations = router({
 				.$if(!!input.detailId, qb =>
 					qb.where('o.detail_id', '=', input.detailId as number),
 				)
+				.selectAll(['o'])
 				.leftJoin('pdo.materials as m', 'o.material_id', 'm.id')
 				.leftJoin('pdo.details as d', 'o.detail_id', 'd.id')
-				.selectAll(['o'])
+				.leftJoin('pdo.orders as ord', 'manufacturing_order_id', 'ord.id')
 				.select('d.name as detail_name')
 				.select('d.logical_group_id as detail_group_id')
 				.select('m.label as material_label')
-				.leftJoin('pdo.orders as manuf', 'manufacturing_order_id', 'manuf.id')
 				.select('o.manufacturing_order_id as manufacturing_order_id')
-				.select('manuf.qty as manufacturing_order_qty')
+				.select('ord.qty as manufacturing_order_qty')
 				.select('m.unit as unit')
 				.orderBy('o.id', 'desc')
 				.limit(Limit)
