ce177c5f6861c8f0f97362f81413e4c39e29721a
Ivan <loseev5@gmail.com>
2026-02-25 14:31:43 +0300

    log

--- server/src/lib/middlewares/error.middleware.ts
+++ server/src/lib/middlewares/error.middleware.ts
@@ -2,6 +2,7 @@ import type { NextFunction, Request, Response } from 'express'
 import { StatusCodes } from 'http-status-codes'
 import { ApiError } from '#root/lib/api.error.js'
 import { Errcode } from '#root/lib/error-code.js'
+import { logger } from '#root/ioc/log.js'
 
 export default function (
   err: ApiError,
@@ -18,6 +19,7 @@ export default function (
     })
   }
 
+  logger.error(err, "Unhandled internal err")
   return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
     error: {
       message: Errcode.UNKNOWN_ERROR_TRY_AGAIN,
